c2b88d2298adb0db119e2993da1bf9ae5008a77c
Irene Scott <irenejinscott@gmail.com>
2020-05-13 01:27:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,26 @@
 class Dog 
-  .name = 
+  def .name = (dogs_name)
+  end 
+  def .name
+    this_dogs_name
+  end
+  def .bark
+    puts "woof!"
+  end
+end
   
-  
+  class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
+ 
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+puts lassie.name
